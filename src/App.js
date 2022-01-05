0011269114b5ebaf89b66f50c18f8caab86b8aed
raffrasson <raf.frasson@gmail.com>
2022-01-04 22:19:49 -0300

    implementa context, provider e a tela de receitas feitas

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AppProvider from './context/AppProvider';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import FinishedRecipes from './pages/FinishedRecipes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/comidas" component={ Home } />
-      </Switch>
+      <AppProvider>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/comidas" component={ Home } />
+          <Route exact path="/receitas-feitas" component={ FinishedRecipes } />
+        </Switch>
+      </AppProvider>
     </BrowserRouter>
   );
 }
