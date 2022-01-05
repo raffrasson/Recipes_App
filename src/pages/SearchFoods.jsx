ae4cb0e6c8cde8ccd882ce0413ac36ac41de779c
allpha23 <allpha23@gmail.com>
2022-01-05 13:31:52 -0300

    adiciona pagina de pesquisa por comidas

---
+++ src/pages/SearchFoods.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../component/Header';
+
+function SearchFoods() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+export default SearchFoods;
