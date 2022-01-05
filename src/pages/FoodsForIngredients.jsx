652845f4b5c419a76506e68752fa1586d181f95b
allpha23 <allpha23@gmail.com>
2022-01-05 13:29:47 -0300

    adiciona pagina de comidas por ingredientes

---
+++ src/pages/FoodsForIngredients.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../component/Header';
+
+function FoodsForIngredients() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+export default FoodsForIngredients;
