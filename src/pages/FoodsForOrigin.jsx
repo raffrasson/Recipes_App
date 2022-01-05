061a8b22f67bf2fe4733f3a62becd43809a5ac8c
allpha23 <allpha23@gmail.com>
2022-01-05 13:30:08 -0300

    adiciona pagina de comidas por origem

---
+++ src/pages/FoodsForOrigin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../component/Header';
+
+function FoodsForOrigin() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+export default FoodsForOrigin;
