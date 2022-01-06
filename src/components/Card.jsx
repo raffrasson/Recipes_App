2b2dd8b0dd46ad7a2b49488faadfc97b17e83088
allpha23 <allpha23@gmail.com>
2022-01-06 19:02:37 -0300

    refatora logica de render no card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,44 +1,43 @@
-/* eslint-disable react/jsx-no-useless-fragment */
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 
 export default function Card() {
   const { recipe } = useContext(AppContext);
 
-  const renderCard = () => {
-    if (recipe === null) return;
-    if (recipe.meals) {
-      return (
-        recipe.meals.map((card, index) => (
-          <div
-            data-testid={ `${index}-recipe-card` }
-            key={ card.idMeal }
-            className="card w-50"
+  const renderCard = () => (
+    recipe.meals.map((card, index) => (
+      <div
+        data-testid={ `${index}-recipe-card` }
+        key={ card.idMeal }
+        className="card w-50"
+      >
+        <img
+          data-testid={ `${index}-card-img` }
+          className="card-img-top"
+          src={ card.strMealThumb }
+          alt={ card.strMeal }
+        />
+        <div className="card-body">
+          <h5
+            data-testid={ `${index}-card-name` }
+            className="card-title"
           >
-            <img
-              data-testid={ `${index}-card-img` }
-              className="card-img-top"
-              src={ card.strMealThumb }
-              alt={ card.strMeal }
-            />
-            <div className="card-body">
-              <h5
-                data-testid={ `${index}-card-name` }
-                className="card-title"
-              >
-                { card.strMeal }
-              </h5>
-            </div>
-          </div>
-        ))
-      );
-    }
-  };
+            { card.strMeal }
+          </h5>
+        </div>
+      </div>
+    ))
+  );
 
   return (
     <div>
-      {
-        renderCard()
+      {recipe.meals && (
+        <div>
+          {
+            renderCard()
+          }
+        </div>
+      )
       }
     </div>
   );
