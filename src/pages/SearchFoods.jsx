36ba678dd8184a2873363a1ea40ead1848b81db3
Erik Lopes <erik.lopesdev@outlook.com>
2022-01-07 16:06:50 -0300

    Adicionando os requisitos 70 ao 74

--- src/pages/SearchFoods.jsx
+++ src/pages/SearchFoods.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 
-function SearchFoods({ history }) {
+function SearchFoods() {
+  const historyRouter = useHistory();
+  const moveToIngredientPage = () => historyRouter.push('/explorar/comidas/ingredientes');
+  const moveToSurprisePage = () => historyRouter.push('/comidas/52771');
+  const moveToAreaPage = () => historyRouter.push('/explorar/comidas/area');
   return (
     <>
       <div>
         <Header />
       </div>
       <div>
-        <Footer history={ history } />
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ moveToIngredientPage }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+          onClick={ moveToAreaPage }
+        >
+          Por Local de Origem
+
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ moveToSurprisePage }
+        >
+          Me Surpreenda!
+
+        </button>
+      </div>
+      <div>
+        <Footer />
       </div>
     </>
   );
 }
-
-SearchFoods.propTypes = {
-  history: PropTypes.objectOf(PropTypes.object).isRequired,
-};
 
 export default SearchFoods;
