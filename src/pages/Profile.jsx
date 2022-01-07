708515642817f4e396c188f21616f4a252e6e24f
Gustavo Souza <gustavogss.dev@gmail.com>
2022-01-07 18:19:47 -0300

    Requisito do Perfil Finalizado

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
+import Data from '../components/Data';
 
 export default function Profile({ history }) {
   return (
     <>
       <div>
         <Header />
+      </div>
+      <div>
+        <Data />
       </div>
       <div>
         <Footer history={ history } />
