414f99eab7695a6eb9de23a659fb8b6f1de7a51b
allpha23 <allpha23@gmail.com>
2022-01-07 19:22:15 -0300

    resolvendo problema de merge

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 
-function Search({ history }) {
-  const historyRouter = useHistory();
+function Search() {
+  const history = useHistory();
 
-  const moveToExploreFood = () => historyRouter.push('/explorar/comidas');
-  const moveToExploreDrinks = () => historyRouter.push('/explorar/bebidas');
+  const moveToExploreFood = () => history.push('/explorar/comidas');
+  const moveToExploreDrinks = () => history.push('/explorar/bebidas');
 
   return (
     <main>
