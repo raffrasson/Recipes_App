a2b817eb0ca063c6d540fb304870d054535c2bfd
Erik Lopes <erik.lopesdev@outlook.com>
2022-01-07 15:26:47 -0300

    Adicionando botao explorar comidas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 
 function Search({ history }) {
+  const historyRouter = useHistory();
+
+  const moveToExploreFood = () => historyRouter.push('/explorar/comida');
+
   return (
-    <>
+    <main>
       <div>
         <Header />
       </div>
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ moveToExploreFood }
+      >
+        Explorar Comidas
+
+      </button>
       <div>
         <Footer history={ history } />
       </div>
-    </>
+    </main>
   );
 }
 
