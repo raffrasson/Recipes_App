5e50986bebc01a954915da21caf556e885b5e6da
allpha23 <allpha23@gmail.com>
2022-01-10 16:10:00 -0300

    correção requisito 10 perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
-import Data from '../components/Data';
 
-export default function Profile({ history }) {
+export default function Profile() {
   return (
     <>
       <div>
         <Header />
       </div>
+
       <div>
-        <Data />
-      </div>
-      <div>
-        <Footer history={ history } />
+        <Footer />
       </div>
     </>
   );
 }
-
-Profile.propTypes = {
-  history: PropTypes.objectOf(PropTypes.object).isRequired,
-};
